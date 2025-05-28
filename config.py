e39947db54189a290525d53282b9e30ec5583902
jigneshsurat009 <jigneshsurat009@gmail.com>
2025-05-28 21:15:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 API_ID = int(os.environ.get("API_ID", "15964777"))
 API_HASH = os.environ.get("API_HASH", "ef448f85b780cdf26f8ffe390a5d8262")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://jigneshsurat009:8s3DYYp0StMLbLjI@cluster0.i8rcb4j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb+srv://online:QokHEErmjX0mIxrO@cluster0.dhai2ja.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 OWNER_ID = int(os.environ.get("OWNER_ID", "944358553"))
